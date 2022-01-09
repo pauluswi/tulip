023a8eb114db209757b9749ac6ec38fdb21a91ab
pauluswi <35646814+pauluswi@users.noreply.github.com>
2022-01-09 11:38:31 +0700

    Update msisdn for test

--- internal/paytoken/api_test.go
+++ internal/paytoken/api_test.go
@@ -16,17 +16,17 @@ func TestAPI(t *testing.T) {
 	logger, _ := log.NewForTest()
 	router := test.MockRouter(logger)
 	repo := &mockRepository{items: []entity.PayToken{
-		{uuid.NewV4().String(), "999999", time.Now(), "081100099", time.Now(), time.Now(), time.Now(), entity.Metadata{time.Now().UTC()}},
+		{uuid.NewV4().String(), "999999", time.Now(), "6281100099", time.Now(), time.Now(), time.Now(), entity.Metadata{time.Now().UTC()}},
 	}}
 	RegisterHandlers(router.Group(""), NewService(repo, logger), auth.MockAuthHandler, logger)
 	header := auth.MockAuthHeader()
 
 	tests := []test.APITestCase{
-		{"get all", "GET", "/getpaytokens/081100099", "", nil, http.StatusOK, `*"Token":"999999"`},
-		{"get unknown", "GET", "/get/paytokens/081100099", "", nil, http.StatusNotFound, ""},
-		{"generate ok", "POST", "/generate", `{"customer_id":"081100099"}`, header, http.StatusCreated, "*valid_until*"},
-		{"generate auth error", "POST", "/generate", `{"customer_id":"081100099"}`, nil, http.StatusUnauthorized, ""},
-		{"generate input error", "POST", "/generate", `"customer_id":"081100099"}`, header, http.StatusBadRequest, ""},
+		{"get all", "GET", "/getpaytokens/6281100099", "", nil, http.StatusOK, `*"Token":"999999"`},
+		{"get unknown", "GET", "/get/paytokens/6281100099", "", nil, http.StatusNotFound, ""},
+		{"generate ok", "POST", "/generate", `{"customer_id":"6281100099"}`, header, http.StatusCreated, "*valid_until*"},
+		{"generate auth error", "POST", "/generate", `{"customer_id":"6281100099"}`, nil, http.StatusUnauthorized, ""},
+		{"generate input error", "POST", "/generate", `"customer_id":"6281100099"}`, header, http.StatusBadRequest, ""},
 		{"validate ok", "POST", "/validate", `{"token":"999999"}`, header, http.StatusCreated, "*valid_until*"},
 		{"validate auth error", "POST", "/validate", `{"CustomerID":"999999"}`, nil, http.StatusUnauthorized, ""},
 		{"validate input error", "POST", "/validate", `"CustomerID":"999999"}`, header, http.StatusBadRequest, ""},
